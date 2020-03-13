6e617ee1e32a9bac1c25960413534a0c75f37ef1
0YSQD05YY1BMCM6\Administrator <huiyao8761380@gmail.com>
2020-03-13 20:53:58 +0800

    2GenMech
    skin modifiers

--- BL_GenMech.py
+++ BL_GenMech.py
@@ -14,7 +14,35 @@ class GenMech(bpy.types.Operator):
         find_object('1GenLine','0AutoMech',"2GenMech")
         #rename_object('GenMech')
         sel = bpy.context.selected_objects
-        for selectobj in sel:
+        for ob in sel:
+            bpy.context.view_layer.objects.active = ob
+            #for mod in [m for m in ob.modifiers if m.type != 'SKIN']:
+
+            #for m in ob.modifiers:
+                #if m.type != 'SKIN':
+            objmod = ob.modifiers.get("Skin")
+            if objmod:
+                ob.modifiers.remove(objmod)
             bpy.ops.object.modifier_add(type='SKIN')
+
+            #for mod in [m for m in ob.modifiers if m.type != 'SKIN']:
+                #bpy.ops.object.modifier_add(modifier = mod.name)
+
+
+            #for mod in [m for m in ob.modifiers if m.type == 'ARMATURE']:
+                #bpy.ops.object.modifier_apply( modifier = mod.name )#应用修改器
+
+        '''
+        #act = bpy.context.active_object
+        genMech_result = bpy.data.collections['2GenMech']
+        if len(genMech_result.objects) > 0:
+            for childObject in genMech_result.objects:
+                if '2GenMech' in childObject.name:
+                    childObject.select_set(True)
+                    bpy.ops.object.modifier_add(type='SKIN')
+
+        #for selectobj in sel:
+            #selectobj=bpy.ops.object.modifier_add(type='SKIN')
+        '''
         self.report({'INFO'}, "2.Gen Mech:bpy.ops.object.bl_genmech()")
         return {'FINISHED'}
